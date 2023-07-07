9851fe3f2a345c0a24ebe9e5b00fb4e82419eb0c
viannejohn <vianneloro@gmail.com>
2023-07-07 11:48:52 +0300

    yeah

--- array_one_value.c
+++ array_one_value.c
@@ -3,6 +3,14 @@
 int main() {
     int grades[18] = {85, 92, 78, 88, 95, 80, 86, 90, 83, 77, 92, 79, 88, 91, 84, 87, 93, 81};
     
-        printf("%d", grades[3]);
+    int studentIndex = 5;  // Example: Printing grade for student at index 5
+    
+    // Check if the student index is within the valid range
+    if (studentIndex >= 0 && studentIndex < 18) {
+        printf("Grade of student %d: %d\n", studentIndex + 1, grades[studentIndex]);
+    } else {
+        printf("Invalid student index.\n");
+    }
+
     return 0;
 }
